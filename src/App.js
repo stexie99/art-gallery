d6ff0e1ea4702d62f028f0821a6ffb001719270a
Steven Xie <xies1999@hotmail.com>
2024-01-01 21:35:38 -0500

    working gallery and button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import Gallery from './views/Gallery'
+import Button from './views/Button'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  let [artId, setArtId] = useState(12720)
+  let [data, setData] = useState({})
+
+  useEffect(() => {
+      document.title='Welcome to Artworld'
+      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setData(resData))
+  }, [artId])
+
+  const handleIterate = (e) => {
+      setArtId(artId + Number(e.target.value))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+        <Button handleIterate = {handleIterate} />
+      </div>
+    </>
+  )
 }
 
 export default App;
